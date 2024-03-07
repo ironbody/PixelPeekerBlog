9e4093b45ca58ffdc0de4763b17ed24dbbe45881
Ironbody <simon.saberian@gmail.com>
2024-03-07 13:40:48 +0100

    Update astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
-
 import react from "@astrojs/react";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), icon()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
     shikiConfig: { theme: "github-dark" },
