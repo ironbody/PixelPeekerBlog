5e59da9e7c4879c785cc77e24fcd460930dfb88f
Ironbody <simon.saberian@gmail.com>
2024-05-26 17:54:56 +0200

    Change theme handling

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useSyncExternalStore } from "react";
 import { Moon, Sun } from "./Icons";
 
 export const themes = {
@@ -7,100 +7,47 @@ export const themes = {
 };
 
 const themeStates = ["light", "dark", "auto"] as const;
-type themeStatesType = (typeof themeStates)[number];
+type ThemeStatesType = (typeof themeStates)[number];
 
 export default function ToggleTheme() {
-  const [currentTheme, setCurrentTheme] = useState<themeStatesType>();
-  const [systemTheme, setSystemTheme] = useState<"light" | "dark">();
+  const { userTheme, setUserTheme, systemTheme } =
+    useTheme<ThemeStatesType>("auto");
 
   useEffect(() => {
     let storedTheme = localStorage.getItem("theme") ?? "auto";
-    if (!themeStates.includes(storedTheme as themeStatesType)) {
+    if (!themeStates.includes(storedTheme as ThemeStatesType)) {
       storedTheme = "auto";
     }
-    changePreference(storedTheme as themeStatesType);
-
-    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    console.log(isDark);
-
-    if (isDark) {
-      setSystemTheme("dark");
-    } else {
-      setSystemTheme("light");
-    }
-
-    const onChange = (e: MediaQueryListEvent) => {
-      if (e.matches) {
-        setSystemTheme("dark");
-      } else {
-        setSystemTheme("light");
-      }
-    };
-
-    addDarkModeListener(onChange);
-
-    return () => {
-      removeDarkModeListener(onChange);
-    };
+    changePreference(storedTheme as ThemeStatesType);
   }, []);
 
   function changePreference(newPref: "light" | "dark" | "auto") {
     switch (newPref) {
       case "auto":
         localStorage.setItem("theme", "auto");
-        setAuto();
+        document.documentElement.removeAttribute("data-theme");
+        setUserTheme("auto");
         break;
       case "light":
         localStorage.setItem("theme", "light");
-        setLight();
+        document.documentElement.dataset.theme = themes.light;
+        setUserTheme("light");
         break;
       case "dark":
         localStorage.setItem("theme", "dark");
-        setDark();
+        document.documentElement.dataset.theme = themes.dark;
+        setUserTheme("dark");
         break;
       default:
         break;
     }
   }
 
-  function addDarkModeListener(listener: (e: MediaQueryListEvent) => void) {
-    console.log("added listener");
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", listener);
-  }
-
-  function removeDarkModeListener(listener: (e: MediaQueryListEvent) => void) {
-    console.log("removed listener");
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .removeEventListener("change", listener);
-  }
-
-  function setLight() {
-    console.log("set light");
-
-    document.documentElement.dataset.theme = themes.light;
-    setCurrentTheme("light");
-  }
-  function setDark() {
-    console.log("set dark");
-    document.documentElement.dataset.theme = themes.dark;
-    setCurrentTheme("dark");
-  }
-
-  function setAuto() {
-    console.log("set auto");
-
-    document.documentElement.removeAttribute("data-theme");
-    setCurrentTheme("auto");
-  }
-
   const isLightMode = (() => {
-    if (currentTheme === "auto") {
+    if (userTheme === "auto") {
       return systemTheme === "light";
     }
-    return currentTheme === "light";
+    return userTheme === "light";
   })();
 
   return (
@@ -145,4 +92,36 @@ export default function ToggleTheme() {
       </ul>
     </div>
   );
+}
+
+function useTheme<TTheme>(initial: TTheme): {
+  userTheme: TTheme;
+  setUserTheme: (t: TTheme) => void;
+  systemTheme: "light" | "dark";
+} {
+  const [userTheme, setUserTheme] = useState<TTheme>(initial);
+
+  const isDark = useSyncExternalStore(
+    (callback) => {
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", callback);
+      return () => {
+        window
+          .matchMedia("(prefers-color-scheme: dark)")
+          .removeEventListener("change", callback);
+      };
+    },
+    () => {
+      return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    },
+  );
+
+  const systemTheme = isDark ? "dark" : "light";
+
+  return {
+    userTheme,
+    setUserTheme,
+    systemTheme,
+  };
 }
