cbb58e581fd0a367cd06a1c06afe0cd29077b7da
Ironbody <simon.saberian@gmail.com>
2024-06-02 15:17:37 +0200

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,21 @@ import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
+import vercelStatic from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), icon()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    shikiConfig: { theme: "github-dark" },
+    shikiConfig: {
+      theme: "github-dark",
+    },
     syntaxHighlight: "shiki",
   },
   prefetch: true,
+  output: "static",
+  adapter: vercelStatic({
+    webAnalytics: { enabled: true },
+  }),
 });
